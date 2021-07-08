596829a91bbebeb96886f7b2de880c423f9cde0f
Radhika <radhika.shankar1220@gmail.com>
2021-07-08 22:02:17 +0530

    [Radhika] Sorting method

--- UnitTestProject/UnitTest1.cs
+++ UnitTestProject/UnitTest1.cs
@@ -1,38 +1,49 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using MaximumProblemUsingGenerics1;
+using Generics_UnitTesting;
+
 namespace UnitTestProject
 {
     [TestClass]
     public class UnitTest1
     {
-       FindMaximumAmongThree<int> maxInt;
+       SortingClass<int> maxInt;
+       SortingClass<float> maxFloat;
 
-        FindMaximumAmongThree<float> maxFloat;
-
-
+        SortingClass<string> maxString;
         [TestInitialize]
         public void SetUp()
         {
-
-            maxInt = new FindMaximumAmongThree<int>(10,20,30);
-            maxFloat = new FindMaximumAmongThree<float>(1.2f, 3.5f, 6.7f);
-
+            string[] arrString = { "Apple", "Banana", "Peach" };
+            maxString = new SortingClass<string>(arrString);
+            int[] arrInt = { 12, 13, 15 };
+            maxInt = new SortingClass<int>(arrInt);
+            float[] arrFloat = { 1.2f, 3.4f, 6.5f };
+            maxFloat = new SortingClass<float>(arrFloat);
         }
         [TestMethod]
         public void TestIntMethod()
         {
 
-            int expected = 30;
-            int actual = maxInt.TestMaximum();
+            int expected = 15;
+            int actual = maxInt.MaxMethod();
             Assert.AreEqual(expected,actual);
         }
         [TestMethod]
         public void TestFloatMethod()
         {
 
-            float expected = 6.7f;
-            float actual = maxFloat.TestMaximum();
+            float expected = 6.5f;
+            float actual = maxFloat.MaxMethod();
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestStringMethod()
+        {
+
+            string expected = "Peach";
+            string actual = maxString.MaxMethod();
             Assert.AreEqual(expected, actual);
         }
     }
