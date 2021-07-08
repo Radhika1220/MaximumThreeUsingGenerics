d600caddd2b807c16bb4e710b04242e49231f1cc
Radhika <radhika.shankar1220@gmail.com>
2021-07-08 22:21:31 +0530

    [Radhika] Final Solution

--- MaximumProblemUsingGenerics1/SortingClass.cs
+++ MaximumProblemUsingGenerics1/SortingClass.cs
@@ -26,10 +26,6 @@ namespace Generics_UnitTesting
             T maxValue = this.MaximumValueFunction(arrayList);
             return maxValue;
         }
-        public void PrintGreatestValue()
-        {
-            var max = MaxMethod();
-            Console.WriteLine("Maximum value is: {0}", max);
-        }
+
     }
 }
